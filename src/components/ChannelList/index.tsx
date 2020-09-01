11679b55a7fb330ebfd08e4ed69135e94577f026
gabrielew <gabrielalcantaratp@gmail.com>
2020-09-01 00:00:58 -0300

    channel list page and styles

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import avatar from '../../images/avatar.jpg'
+
+import {
+  List,
+  ChannelContainer,
+  LeftSide,
+  AvatarContainer,
+  Avatar,
+  Column,
+  Username,
+  Info,
+  RightSide,
+  WhiteCircle,
+} from './styles';
+
+const ChannelList: React.FC = () => {
+  const ChannelItem = () => (
+    <ChannelContainer>
+      <LeftSide>
+        <AvatarContainer>
+          <Avatar source={avatar} />
+        </AvatarContainer>
+
+        <Column >
+          <Username></Username>
+          <Info>36 news videos</Info>
+        </Column>
+      </LeftSide>
+
+      <RightSide>
+        <WhiteCircle />
+      </RightSide>
+    </ChannelContainer>
+  )
+
+  return (
+    <List>
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+    </List>
+  );
+};
+
+export default ChannelList;
