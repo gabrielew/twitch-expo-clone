437534e94953a4ad407727b766c8f091a3a84ec9
gabrielew <gabrielalcantaratp@gmail.com>
2020-08-31 23:59:51 -0300

    creating export default

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from './Header'
+export { default as Heading } from './Heading'
+export { default as Title } from './Title'
+export { default as CategoryList } from './CategoryList'
+export { default as StreamList } from './StreamList'
+export { default as ChannelList } from './ChannelList'
